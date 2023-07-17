ee5c7a839b16366676800473a1768ea6499e999c
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-07-17 21:05:03 +0700

    add new task

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addInput = document.querySelector(".add-input");
 const addButton = document.querySelector("form button");
+const taskList = document.querySelector(".list");
+let taskArr = [];
 
 // Activate and deactivate input button
 addInput.addEventListener("keyup", (e) => {
@@ -11,3 +13,51 @@ addInput.addEventListener("keyup", (e) => {
         addButton.classList.remove("active");
     }
 });
+
+// Add task to list
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let userData = addInput.value;
+    let getLocalStorage = localStorage.getItem("Todo");
+    if (getLocalStorage === null) {
+        taskArr = [];
+    } else {
+        taskArr = JSON.parse(getLocalStorage);
+    }
+    taskArr.push(userData);
+    localStorage.setItem("Todo", JSON.stringify(taskArr));
+    showTasks();
+    addButton.classList.remove("active");
+    addInput.value = "";
+})
+
+function showTasks() {
+    let getLocalStorage = localStorage.getItem("Todo");
+    if (getLocalStorage === null) {
+        taskArr = [];
+    } else {
+        taskArr = JSON.parse(getLocalStorage);
+    }
+
+    // task counter
+    let footerMessage = document.querySelector(".footer > span");
+    console.log(footerMessage);
+    if (taskArr.length === 0) {
+        footerMessage.textContent = "You don't have any tasks";
+    } else {
+        const pendingNum = taskArr.length;
+        let taskType = "task";
+        if (taskArr.length > 1) {
+            taskType = "tasks";
+        }
+        footerMessage.textContent = `You have ${pendingNum} pending ${taskType}`;
+    }
+    
+
+    // show tasks as list items
+    let newTask = "";
+    taskArr.forEach((element, index) => {
+        newTask += `<li>${element} <span onclick="deleteTask(${index})"><i class="fas fa-trash"></i></li>`
+    });
+    taskList.innerHTML = newTask;
+}
